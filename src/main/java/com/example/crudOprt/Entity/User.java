7d2d098e3a001a57bcade7190748b71ca7934a69
Sagar <sagarkumbhalkar2019@gmail.com>
2026-01-01 22:45:55 +0530

    Added the missed Annotations

--- src/main/java/com/example/crudOprt/Entity/User.java
+++ src/main/java/com/example/crudOprt/Entity/User.java
@@ -1,18 +1,20 @@
 package com.example.crudOprt.Entity;
 
 import com.example.crudOprt.Enums.Role;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String first_name;
     private String last_name;
