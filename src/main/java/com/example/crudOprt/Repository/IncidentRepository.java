5d1625c7e1b1d50c347b11895fced5ad268d3be3
Sk264 <sdkumbhalkar07@gmail.com>
2026-01-01 22:23:16 +0530

    Added the required Annotations

--- src/main/java/com/example/crudOprt/Repository/IncidentRepository.java
+++ src/main/java/com/example/crudOprt/Repository/IncidentRepository.java
@@ -2,6 +2,8 @@ package com.example.crudOprt.Repository;
 
 import com.example.crudOprt.Entity.Incident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
 }
