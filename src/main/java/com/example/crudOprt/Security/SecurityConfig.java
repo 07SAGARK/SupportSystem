03c9b992f183c11b8085f62695a94ea367d5caa9
Sagar <sagarkumbhalkar2019@gmail.com>
2026-01-03 23:37:17 +0530

    Added some security relates code in SecurityConfig file

--- src/main/java/com/example/crudOprt/Security/SecurityConfig.java
+++ src/main/java/com/example/crudOprt/Security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.crudOprt.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,5 +12,14 @@ public class SecurityConfig  {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/agent/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
     }
 }
