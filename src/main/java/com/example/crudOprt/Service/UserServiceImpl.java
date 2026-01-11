72af6e91ac7e470831cdd93d43cb93b70654db94
Sagar <sagarkumbhalkar2019@gmail.com>
2026-01-11 20:35:35 +0530

    Working on performing the Login

--- src/main/java/com/example/crudOprt/Service/UserServiceImpl.java
+++ src/main/java/com/example/crudOprt/Service/UserServiceImpl.java
@@ -7,6 +7,10 @@ import com.example.crudOprt.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,15 +18,17 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 @Service
-public class UserServiceImpl{
+public class UserServiceImpl implements UserDetailsService {
 
     private UserRepository repository;
     private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
 
     @Autowired
-    public UserServiceImpl(UserRepository repository,PasswordEncoder passwordEncoder){
+    public UserServiceImpl(UserRepository repository,PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.repository=repository;
         this.passwordEncoder=passwordEncoder;
+        this.authenticationManager=authenticationManager;
     }
     public ResponseEntity<?> addUser(User user){
         if (repository.findByEmail(user.getEmail()).isPresent()){
@@ -34,15 +40,13 @@ public class UserServiceImpl{
         repository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-    public ResponseEntity<?> login(LoginRequest request){
-        User user=repository.findByEmail(request.getUsername()).orElseThrow(()-> new UsernameNotFoundException("Invalid Username"));
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-            throw new RuntimeException("Wrong Password");
-        }
-        return ResponseEntity.ok(Map.of(
-                "message","Login Successful",
-                "email",user.getEmail()
-        ));
-    }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=repository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("Invalid Username"));
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
 }
