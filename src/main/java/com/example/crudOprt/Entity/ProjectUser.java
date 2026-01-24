83d90a20ba2b3c6d57128e09a4afbfaf4cab4fd2
Sagar <sagarkumbhalkar2019@gmail.com>
2026-01-24 20:33:49 +0530

    Updated some fields and Logic related to the Incident

--- src/main/java/com/example/crudOprt/Entity/ProjectUser.java
+++ src/main/java/com/example/crudOprt/Entity/ProjectUser.java
@@ -1,6 +1,7 @@
 package com.example.crudOprt.Entity;
 
 import com.example.crudOprt.Enums.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,7 +20,10 @@ public class ProjectUser {
     private String first_name;
     private String last_name;
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // this helps to make the password write only which prevents it to get exposed in the client side or in the network
     private String password;
+
     @Enumerated(EnumType.STRING)
     private Role role;
 }
