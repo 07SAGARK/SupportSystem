13f2f1c40338d412238e1115d388799decf479ca
Sagar <sagarkumbhalkar2019@gmail.com>
2026-02-08 22:57:26 +0530

    Established the Role Based redirection for Home Page

---
+++ src/main/java/com/example/crudOprt/Service/CustomLoginSuccessHandler.java
@@ -0,0 +1,46 @@
+package com.example.crudOprt.Service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Configuration
+public class CustomLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String url=determineUrl(authentication);
+        if (response.isCommitted()){
+            return;
+        }
+        RedirectStrategy redirectStrategy=new DefaultRedirectStrategy();
+        redirectStrategy.sendRedirect(request, response, url);
+    }
+
+    protected String determineUrl(Authentication authentication){
+        String url="/login?error=true";
+        Collection<? extends GrantedAuthority> authorities=authentication.getAuthorities();
+        List<String> roles=new ArrayList<>();
+        for (GrantedAuthority a: authorities){
+            roles.add(a.getAuthority());
+        }
+        System.out.println(roles);
+        if (roles.contains("ROLE_AGENT") || roles.contains("ROLE_ADMIN")){
+            System.out.println("Agent role is selected");
+            url="/agent/Home/homePageAdmin";
+        }else if (roles.contains("ROLE_CLIENT")){
+            System.out.println("Client role is selected");
+            url="/agent/Home/homePageClient";
+        }
+        System.out.println(url);
+        return url;
+    }
+}
